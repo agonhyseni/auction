bca5c694b8724490aece46a48eb551d3d69cc1d9
Agon Hyseni <hyseniagon@gmail.com>
2020-09-17 13:06:24 +0200

    Added set auction picture url to dynamo db function

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,6 +3,7 @@ import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
 import { getAuctionById } from "./getAuction";
 import { uploadPictureToS3 } from "../lib/uploadPicture";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
@@ -11,12 +12,10 @@ export async function uploadAuctionPicture(event) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
+  let updatedAuction;
   try {
-    const uploadToS3Result = await uploadPictureToS3(
-      auction.id + ".jpg",
-      buffer
-    );
-    console.log(uploadToS3Result);
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -24,7 +23,7 @@ export async function uploadAuctionPicture(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
